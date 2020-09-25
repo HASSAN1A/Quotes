b86410afcceb85954f5b2802d84df9e56c0dc677
Hassan Juma <okothhassanjuma@gmail.com>
2020-09-26 02:31:35 +0300

    add modified module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
+import { HighlighterDirective } from './highlighter.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+// import { from } from 'rxjs'
+import { TimeCountPipe } from './time-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuotesDetailComponent,
+    HighlighterDirective,
+    DateCountPipe,
+    QuotesFormComponent,
+    TimeCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
