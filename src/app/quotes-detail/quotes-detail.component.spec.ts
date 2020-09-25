a1027fdaa98aa5697bd8bfcc6f47f1f1c7664124
Hassan Juma <okothhassanjuma@gmail.com>
2020-09-26 02:38:45 +0300

    test detail add modified

---
+++ src/app/quotes-detail/quotes-detail.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuotesDetailComponent } from './quotes-detail.component';
+
+describe('QuotesDetailComponent', () => {
+  let component: QuotesDetailComponent;
+  let fixture: ComponentFixture<QuotesDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ QuotesDetailComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuotesDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
